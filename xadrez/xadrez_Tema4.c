1fab517e9c27c262cab05e5b688fb98c0d7e052a
Ane Matias <annye.loves2@gmail.com>
2026-02-03 13:58:11 +0000

    nvl aventureiro

--- xadrez/xadrez_Tema4.c
+++ xadrez/xadrez_Tema4.c
@@ -22,13 +22,34 @@ int main(){
     }
 
 
-    //RAINHA (USANDO DO WHILE)
+    // RAINHA (USANDO DO WHILE)
     printf("\n=== RAINHA ===\n");
     int k=1;
     do{
         printf("Movimento %d -> Esquerda\n", k);
         k++;
     } while (k <= rainha);
+
+    // CAVALO (USANDO LOOPS ANINHADOS)
+    printf("\n=== CAVALO ===\n");
+    int baixo=2;
+    int esquerda=1;
+
+    // LOOP EXTERNO FOR 
+    for (int i=1; i<=baixo; i++){
+    printf("Movimento %d -> Baixo\n", i);
+
+        // LOOP INTERNO WHILE 
+        if (i== baixo){
+            int l=1;
+            while (l<= esquerda){
+                printf("Movimento %d -> Esquerda\n", l+2);
+                l++;
+                
+            }
+        } 
+
+    }
 
     return 0;
 }
